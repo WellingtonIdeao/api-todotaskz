22faa52e43ad6cab663aa4d9c8b115a7e8f833c6
Wellington Ideão <wellington.ideao@gmail.com>
2022-08-08 13:27:12 -0300

    Change ModelSerializer to HyperLinkedModelSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,14 +2,14 @@ from rest_framework import serializers
 from .models import Category, Task
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Category
-        fields = ['id', 'title', 'created', 'updated', 'tasks']
+        fields = ['url', 'id', 'title', 'created', 'updated', 'tasks']
 
 
-class TaskSerializer(serializers.ModelSerializer):
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
-        depth = 1
